1814579b34021cdd0ee9737fed474aef33e8b5df
Daniel mandelel <daneil9.man@gmail.com>
2021-11-16 00:32:45 +0200

    first night

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://awakmsnbccrbfy:f3b3e4e1d86f1a2722f8011507c6ce84af6da8d812e6c7537266f32236401cb4@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/dar0ufs7rl9a5h'
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+except:
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'] = 'postgresql://awakmsnbccrbfy:f3b3e4e1d86f1a2722f8011507c6ce84af6da8d812e6c7537266f32236401cb4@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/dar0ufs7rl9a5h'
+    print("not local DB")
 db = SQLAlchemy(app)
 
 
@@ -18,19 +23,33 @@ class User(db.Model):
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender = db.Column(db.String(80), nullable=False)
-    reciver = db.Column(db.String(80), nullable=False)
+    receiver = db.Column(db.String(80), nullable=False)
     mess = db.Column(db.String(80), nullable=False)
     subject = db.Column(db.String(80), nullable=False)
     time = db.Column(db.String(80), nullable=False)
+    is_read = db.Column(db.Boolean)
+
+    def print_all(self):
+        return {"id": self.id, "sender": self.sender, "receiver": self.receiver, "mess": self.mess,
+                "subject": self.subject, "time": self.time, "is_read": self.is_read}
+
+    def read(self):
+        self.is_read = True
+        db.session.commit()
 
 
 @app.route('/')
 # TODO def show_all_mess():
 def show_all_mess():
-    a = Message.query.all()
-    x = [n.mess for n in a]
-    print(a)
-    print(x)
+    sender = request.form["sender"]
+    all = Message.query.filter(Message.receiver == sender).all()
+    x = []
+    for mess in all:
+        mess.read()
+        x.append(mess.print_all())
+    # x = [mess.is_read==True , for mess in all]
+    # print(a)
+    # print(x)
     return str(x)
 
 
@@ -42,16 +61,21 @@ def send_mess():
     subject = request.form["subject"]
     mess = request.form['mess']
     date = datetime.datetime.now()
-    print(date)
-    message = Message(sender=sender, reciver=receiver, mess=mess, subject=subject, time=str(date))
+    # print(str(date))
+    message = Message(sender=sender, receiver=receiver, mess=mess, subject=subject, time=str(date), is_read=False)
     db.session.add(message)
     db.session.commit()
     return message.mess
 
 
 @app.route('/show_all_unreaded_mess')
-def show_all_unreaded_mess(sender, reciver, mess, subject, date):
-    return "200"
+def show_all_unreaded_mess():
+    x=[]
+    sender = request.form["sender"]
+    all = Message.query.filter(Message.is_read == False).filter( Message.receiver == sender)
+    print(all)
+    x = [mess.print_all() for mess in all]
+    return str(x)
 
 
 @app.route('/read_mess')
@@ -60,8 +84,9 @@ def read_mess(sender, reciver, mess, subject, date):
 
 
 @app.route('/delete_mess')
-def delete_mess(sender, reciver, mess, subject, date):
-    return
+def delete_mess():
+    Message.__table__.drop(db.engine)
+    return "ok"
 
 
 if __name__ == '__main__':
